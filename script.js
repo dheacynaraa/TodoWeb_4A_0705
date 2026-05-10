f0700f81965c35dd9833c70d07e3b3eb1d43c045
dheacynaraa <dheacynaraa@gmail.com>
2026-05-10 23:29:12 +0700

    Add Action Elements

--- script.js
+++ script.js
@@ -12,11 +12,17 @@ btnAdd.addEventListener("click", function () {
 
   let newList = document.createElement("li");
 
-  newList.innerHTML = `<div class="task-container">
-            <div class="task">${textTask}</div>
-            <div class="status">On Progress</div>
-        </div>
-        `;
+  newList.innerHTML = 
+  `<div class="task-container">
+        <div class="task">${textTask}</div>
+        <div class="status">On Progress</div>
+    </div>
+
+    <div class="actions">
+        <i class="fas fa-check"></i>
+        <i class="fas fa-edit"></i>
+        <i class="fas fa-trash"></i>
+    </div>`;
 
   taskList.appendChild(newList);
 
